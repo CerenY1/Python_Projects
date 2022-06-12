8c323ac89f95c364026accfe49152e141263816f
CerenY1 <94191821+CerenY1@users.noreply.github.com>
2022-06-12 13:30:42 +0300

    My Python projects from basic to complex

---
+++ basit_kumanda.py
@@ -0,0 +1,147 @@
+
+import random
+import time
+
+class Kumanda():
+
+    def __init__(self,tv_durumu="Kapalı",ses_durumu=0,kanal_listesi=["Trt"],kanal="Trt"):
+
+        self.tv_durumu = tv_durumu
+        self.ses_durumu = ses_durumu
+        self.kanal_listesi = kanal_listesi
+        self.kanal = kanal
+
+    def tv_aç(self):
+
+       if self.tv_durumu == "Açık":
+           print("Televizyon zaten açık...")
+
+       else:
+           print("Televizyon açılıyor...")
+           self.tv_durumu="Açık"
+
+    def tv_kapat(self):
+
+        if self.tv_durumu == "Kapalı":
+            print("Televizyon zaten kapalı...")
+
+        else:
+            print("televizyon kapatılıyor...")
+            self.tv_durumu="Kapalı"
+
+    def ses_ayarları(self):
+
+        while True:
+
+            ses = input("Sesi yükseltmek için :> \n tuşuna azaltmak için :< tuşuna basınız.")
+            if ses =="<":
+                if self.ses_durumu!=0:
+                   self.ses_durumu-=1
+                   print("Ses:",self.ses_durumu)
+
+
+            elif ses == ">":
+                if self.ses_durumu!=31:
+                    self.ses_durumu+=1
+                    print("Ses:",self.ses_durumu)
+
+            else:
+                print("Güncel ses:",self.ses_durumu)
+
+                break
+
+    def kanal_ekle(self,kanal_ismi):
+
+
+         print("Kanallar güncelleniyor...")
+         time.sleep(1)
+         self.kanal_listesi.append(kanal_ismi)
+         print("Kanal eklendi...")
+
+    def kanallar(self):
+        print("Güncel Kanal Listesi: ",self.kanal_listesi)
+
+    def rastgele_kanal(self):
+
+        rastgele=random.randint(0,len(self.kanal_listesi)-1)
+        self.kanal=self.kanal_listesi[rastgele]
+
+        print("Kanal:",self.kanal)
+
+    def __len__(self):
+        return len(self.kanal_listesi)
+
+    def __str__(self):
+        return "Tv Durumu:{} \n Ses Durumu:{} \n Kanal:{} \n Kanal Listesi:{}".format(self.tv_durumu,self.ses_durumu,self.kanal,self.kanal_listesi)
+
+kumanda=Kumanda()
+
+print("""
+Kumanda Uygulaması
+
+1)Tv aç
+2)Tv kapat
+3)Ses ayarları
+4)Kanal ekleme
+5)Kanal sayısı
+6)Kanal listesi
+7)Rastgele kanala geçme
+8)Televizyon bilgileri
+
+"""
+
+)
+
+while True:
+    işlem = int(input("İşlem:"))
+
+    if işlem==1:
+        kumanda.tv_aç()
+
+    elif işlem==2:
+        kumanda.tv_kapat()
+
+    elif işlem==3:
+        kumanda.ses_ayarları()
+
+    elif işlem==4:
+        kanal_isimleri = input("Kanal isimlerini ',' ile ayırarak girin:")
+
+        kanal_listesi = kanal_isimleri.split(",")
+
+        for eklenecekler in kanal_listesi:
+            kumanda.kanal_ekle(eklenecekler)
+
+    elif işlem==5:
+        print("Kanal Sayısı:",len(kumanda))
+
+    elif işlem==6:
+        kumanda.kanallar()
+
+    elif işlem==7:
+        kumanda.rastgele_kanal()
+
+    elif işlem==8:
+        print(kumanda)
+
+    else:
+        print("Geçersiz işlem...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
